4fbd9015bc6657875be05acc19f28e9ed9a15b7f
Rifat Khan <rifat6633@gmail.com>
2024-04-08 13:03:15 +0600

    add: Coordinates and Interactivity1

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import P5js from "@/components/p5js";
+import Coordinates from "@/components/p5js/01_Coordinates";
+import Interactivity1 from "@/components/p5js/02_interactivity1";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-10 py-10">
+      <Interactivity1 />
+
+      <Coordinates />
+
       <P5js />
     </main>
   );
